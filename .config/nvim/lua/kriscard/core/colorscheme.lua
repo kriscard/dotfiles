993c56755df8968155c5e0eb11e7d5d8d625f588
Christopher Cardoso <christopher@northone.com>
2023-03-27 00:36:10 -0400

    use catppuccin macchiato style

--- .config/nvim/lua/kriscard/core/colorscheme.lua
+++ .config/nvim/lua/kriscard/core/colorscheme.lua
@@ -1,6 +1,6 @@
 -- set colorscheme to tokyonight with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, 'colorscheme catppuccin')
+local status, _ = pcall(vim.cmd, 'colorscheme catppuccin-macchiato')
 if not status then
 	print("Colorscheme not found!")
   return
