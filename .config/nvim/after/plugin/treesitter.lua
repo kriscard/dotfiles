7f7f5e9497c433cf0308e087018abbea43b463c3
kriscard <cardoso.christopher01@gmail.com>
2023-06-01 18:32:47 +0000

    nvim: update treesitter config

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -8,33 +8,42 @@ if not instance then
   return
 end
 
-
-
 treesitter.setup {
-  -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "typescript", "javascript", "lua", "json", "tsx", "yaml", "html", "css", "graphql", "vim",
-    "gitignore" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    disable = {},
   },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "markdown",
+    "markdown_inline",
+    "tsx",
+    "typescript",
+    "toml",
+    "json",
+    "yaml",
+    "css",
+    "html",
+    "lua",
+    "yaml",
+    "graphql",
+    "vim",
+    "javascript"
+  },
+  autotag = {
+    enable = true,
+  },
+  context_commentstring = {
+    enable         = true,
+    enable_autocmd = false,
+  }
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 
 treesitterContext.setup({
   enable = true
