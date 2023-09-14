31b59eedb7aa29def85bad1a7747b88f2bc653d8
kriscard <contact@christophercardoso.dev>
2023-09-14 00:25:34 -0400

    Update Tmux config

--- .config/nvim/lua/kriscard/plugins/bufferline.lua
+++ .config/nvim/lua/kriscard/plugins/bufferline.lua
@@ -5,15 +5,7 @@ return {
 	version = "*",
 	opts = {
 		options = {
-			buffer_close_icon = "",
 			close_icon = "",
-			offsets = {
-				{
-					filetype = "NvimTree",
-					text = "Nvim tree",
-					highlight = "FileExplorer",
-				},
-			},
 			separator_style = "slant",
 		},
 	},
