fb59fc08c20fdbeb8feae209844e924dffaa462d
kriscard <contact@christophercardoso.dev>
2024-01-16 22:09:52 -0500

    add new config for sketchybar

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# Battery is here bcause the ICON_COLOR doesn't play well with all background colors
+source "$HOME/.config/sketchybar/icons.sh"
+source "$HOME/.config/sketchybar/colors.sh"
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
@@ -9,35 +10,33 @@ if [ $PERCENTAGE = "" ]; then
 	exit 0
 fi
 
+DRAWING=on
+COLOR=$WHITE
 case ${PERCENTAGE} in
-[8-9][0-9] | 100)
-	ICON=""
-	ICON_COLOR=0xffa6da95
+9[0-9] | 100)
+	ICON=$BATTERY_100
+	DRAWING=off
 	;;
-7[0-9])
-	ICON=""
-	ICON_COLOR=0xffeed49f
+[6-8][0-9])
+	ICON=$BATTERY_75
+	DRAWING=off
 	;;
-[4-6][0-9])
-	ICON=""
-	ICON_COLOR=0xfff5a97f
+[3-5][0-9])
+	ICON=$BATTERY_50
 	;;
-[1-3][0-9])
-	ICON=""
-	ICON_COLOR=0xffee99a0
+[1-2][0-9])
+	ICON=$BATTERY_25
+	COLOR=$ORANGE
 	;;
-[0-9])
-	ICON=""
-	ICON_COLOR=0xffed8796
+*)
+	ICON=$BATTERY_0
+	COLOR=$RED
 	;;
 esac
 
 if [[ $CHARGING != "" ]]; then
-	ICON=""
-	ICON_COLOR=0xffeed49f
+	ICON=$BATTERY_CHARGING
+	DRAWING=off
 fi
 
-sketchybar --set $NAME \
-	icon=$ICON \
-	label="${PERCENTAGE}%" \
-	icon.color=${ICON_COLOR}
+sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
--- .zshrc
+++ .zshrc
@@ -84,3 +84,4 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 # pnpm end
+export PATH=$PATH:/Users/kriscard/.spicetify
