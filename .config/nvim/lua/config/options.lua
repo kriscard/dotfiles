d3701695e23599fd0c2a064d522d8e8629597fce
kriscard <contact@christophercardoso.dev>
2024-02-27 23:20:41 -0500

    setup lazy and add catppuccin

--- .config/nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Desactivate the arrow, THIS IS THE WAY
-vim.keymap.set("n", "<up>", "<nop>", { silent = true })
-vim.keymap.set("n", "<down>", "<nop>", { silent = true })
-vim.keymap.set("n", "<left>", "<nop>", { silent = true })
-vim.keymap.set("n", "<right>", "<nop>", { silent = true })
