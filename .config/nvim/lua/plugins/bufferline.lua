2a619574448b42783c58729ffecf7bca1bad573c
kriscard <contact@christophercardoso.dev>
2024-03-05 11:09:42 -0500

    Add diagnostic icons for bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,11 @@
+local iconsList = {
+	diagnostics = {
+		Error = " ",
+		Warn = " ",
+		Hint = " ",
+		Info = " ",
+	},
+}
 return {
 	"akinsho/bufferline.nvim",
 	event = "VeryLazy",
@@ -21,7 +29,7 @@ return {
 			diagnostics = "nvim_lsp",
 			always_show_bufferline = false,
 			diagnostics_indicator = function(_, _, diag)
-				local icons = require("lazyvim.config").icons.diagnostics
+				local icons = iconsList.diagnostics
 				local ret = (diag.error and icons.Error .. diag.error .. " " or "")
 					.. (diag.warning and icons.Warn .. diag.warning or "")
 				return vim.trim(ret)
