5697648b565cd9bb2a93b3c5dba38e8d7a515e44
kriscard <contact@christophercardoso.dev>
2024-03-06 21:52:01 -0500

    fix supertab in nvim cmp

--- .config/nvim/lua/plugins/nvim_cmp.lua
+++ .config/nvim/lua/plugins/nvim_cmp.lua
@@ -19,6 +19,13 @@ return {
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local autopairs = require("nvim-autopairs")
 
+		-- Local function for SuperTab see https://www.lazyvim.org/configuration/recipes#supertab
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		-- Load autopairs
 		autopairs.setup({})
 
@@ -97,7 +104,7 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-						cmp.select_next_item({ select = true })
+						cmp.select_next_item()
 					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
 					-- this way you will only jump inside the snippet region
 					elseif luasnip.expand_or_jumpable() then
