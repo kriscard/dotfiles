c030459bb3df2b027585f468dbab45978f0afb02
Chris Cardoso <chris.ca@getjobber.com>
2024-03-12 21:47:46 -0400

    fix nvim cmp keybindings

--- .config/nvim/lua/plugins/nvim_cmp.lua
+++ .config/nvim/lua/plugins/nvim_cmp.lua
@@ -93,8 +93,8 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-j>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-u>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
