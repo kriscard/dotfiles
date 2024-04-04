40a5a889b9232ac31822d3c0c7a1e6b7903ccb04
Chris Cardoso <chris.ca@getjobber.com>
2024-04-04 12:58:48 -0400

    feat: clean useless comment

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,14 +49,6 @@ return {
 			tailwindcss = {},
 			tsserver = {},
 			yamlls = {},
-			-- eslint = {
-			-- 	on_attach = function(_, bufnr)
-			-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 			buffer = bufnr,
-			-- 			command = "EslintFixAll",
-			-- 		})
-			-- 	end,
-			-- },
 		}
 
 		-- Default handlers for LSP
