4318ab08e1fd48b976939fccd8d171095a3d47e9
chriscardoso <chriscardoso@chrisCardosoJobber.local>
2024-05-15 13:49:00 -0400

    feat: Add nvim dap and update lazygit config

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,22 +1,15 @@
+-- nvim v0.8.0
 return {
   "kdheepak/lazygit.nvim",
-  event = "VeryLazy",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  config = function()
-    require("lazy").setup({
-      {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-          "LazyGit",
-          "LazyGitConfig",
-          "LazyGitCurrentFile",
-          "LazyGitFilter",
-          "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-      },
-    })
-  end,
 }
