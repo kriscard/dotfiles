ae7043dd07e3a9855286a7555ca8da13cfbb0a1e
chriscardoso <chriscardoso@chrisCardosoJobber.local>
2024-05-21 12:57:57 -0400

    feat: Add lazyvim

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,6 +1,6 @@
 return {
   "nvimdev/dashboard-nvim",
-  event = "VimEnter",
+  lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
   opts = function()
     local logo = [[
 	  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
@@ -26,13 +26,15 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
-          { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
-          { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-          { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-          { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-          { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-          { action = "qa", desc = " Quit", icon = " ", key = "q" },
+          { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+          { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
         },
         footer = function()
           local stats = require("lazy").stats()
--- .config/nvim/lua/plugins/mini_bufremove.lua
+++
@@ -1,2 +0,0 @@
--- Plugins to remove open buffers see keybindings in Bufferline config
-return { "echasnovski/mini.bufremove", version = false, event = "VeryLazy" }
