5262ab17b090c9674092fcd503959bb75dd26ff3
chriscardoso <chriscardoso@chrisCardosoJobber.local>
2024-05-21 14:02:11 -0400

    Feat: Add Telescope keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -102,61 +102,57 @@ return {
       require("telescope.builtin").find_files(opts)
     end
 
-    -- -- File Pickers
-    -- vim.keymap.set("n", "<leader>ff", find_files_from_current_dir, { desc = "Find Files from current directory" })
-    --
-    -- vim.keymap.set("n", "<leader>fF", find_files_from_root_dir, { desc = "Find Files from root directory" })
-    -- vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Fuzzy search through the output of git ls-files" })
-    -- vim.keymap.set(
-    --   "n",
-    --   "<leader>ss",
-    --   builtin.grep_string,
-    --   { desc = "Searches for the string under your cursor or selection in your current working directory" }
-    -- )
-    -- vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Grep (root dir)" })
-    -- vim.keymap.set(
-    --   "n",
-    --   "<leader>//",
-    --   telescope_extensions.live_grep_args.live_grep_args,
-    --   { noremap = true, desc = "Grep (root dir) with args" }
-    -- )
-    --
-    -- -- Vim Pickers
-    -- vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Lists open buffers in current neovim instance" })
-    -- vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Lists previously open files" })
-    -- vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
-    -- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
-    -- vim.keymap.set("n", "<leader>ui", builtin.colorscheme, { desc = "Lists available colorschemes" })
-    -- vim.keymap.set("n", "<leader>sq", builtin.quickfix, { desc = "Lists items in the quickfix list" })
-    -- vim.keymap.set("n", "<leader>sj", builtin.jumplist, { desc = "Lists Jump List entries" })
-    -- vim.keymap.set("n", "<leader>sR", builtin.registers, { desc = "Lists vim registers" })
-    --
-    -- -- Neovim LSP Pickers
-    -- vim.keymap.set("n", "<leader>R", builtin.lsp_references, { desc = "Lists vim registers" })
-    -- vim.keymap.set(
-    --   "n",
-    --   "<leader>sd",
-    --   builtin.diagnostics,
-    --   { desc = "Lists Diagnostics for all open buffers or a specific buffer" }
-    -- )
-    -- vim.keymap.set(
-    --   "n",
-    --   "gi",
-    --   builtin.lsp_implementations,
-    --   { desc = "Goto the implementation of the word under the cursor" }
-    -- )
-    -- vim.keymap.set(
-    --   "n",
-    --   "gd",
-    --   builtin.lsp_definitions,
-    --   { desc = "Goto the implementation of the word under the cursor" }
-    -- )
-    --
-    -- -- Git Pickers
-    -- vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Lists buffer's git commits with diff preview" })
-    -- vim.keymap.set("n", "<leader>gS", builtin.git_stash, { desc = "Lists stash items in current repository" })
-    --
-    -- -- Treesitter Picker
-    -- vim.keymap.set("n", "<leader>tt", builtin.treesitter, { desc = "Lists fn names, variables from Treesitter" })
+    -- File Pickers
+    vim.keymap.set("n", "<leader>ff", find_files_from_current_dir, { desc = "Find Files from current directory" })
+
+    vim.keymap.set("n", "<leader>fF", find_files_from_root_dir, { desc = "Find Files from root directory" })
+    vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Fuzzy search through the output of git ls-files" })
+    vim.keymap.set(
+      "n",
+      "<leader>ss",
+      builtin.grep_string,
+      { desc = "Searches for the string under your cursor or selection in your current working directory" }
+    )
+    vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Grep (root dir)" })
+    vim.keymap.set(
+      "n",
+      "<leader>//",
+      telescope_extensions.live_grep_args.live_grep_args,
+      { noremap = true, desc = "Grep (root dir) with args" }
+    )
+
+    -- Vim Pickers
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Lists open buffers in current neovim instance" })
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Lists previously open files" })
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
+    vim.keymap.set("n", "<leader>sq", builtin.quickfix, { desc = "Lists items in the quickfix list" })
+    vim.keymap.set("n", "<leader>sj", builtin.jumplist, { desc = "Lists Jump List entries" })
+    vim.keymap.set("n", "<leader>sR", builtin.registers, { desc = "Lists vim registers" })
+
+    -- Neovim LSP Pickers
+    vim.keymap.set("n", "<leader>R", builtin.lsp_references, { desc = "Lists vim registers" })
+    vim.keymap.set(
+      "n",
+      "<leader>sd",
+      builtin.diagnostics,
+      { desc = "Lists Diagnostics for all open buffers or a specific buffer" }
+    )
+    vim.keymap.set(
+      "n",
+      "gi",
+      builtin.lsp_implementations,
+      { desc = "Goto the implementation of the word under the cursor" }
+    )
+    vim.keymap.set(
+      "n",
+      "gd",
+      builtin.lsp_definitions,
+      { desc = "Goto the implementation of the word under the cursor" }
+    )
+
+    -- Git Pickers
+    vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Lists buffer's git commits with diff preview" })
+    vim.keymap.set("n", "<leader>gS", builtin.git_stash, { desc = "Lists stash items in current repository" })
   end,
 }
