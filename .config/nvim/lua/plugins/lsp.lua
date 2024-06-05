d1968ad20c91a6d4444106da8e2fc926919e0c17
chriscardoso <chriscardoso@chrisCardosoJobber.local>
2024-06-04 21:33:56 -0400

    deps(lsp): downgrade to eslint 4.8.0

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -29,8 +29,8 @@ return {
 
     null_ls.setup({
       sources = {
-        require("none-ls.diagnostics.eslint_d"),
-        require("none-ls.code_actions.eslint_d"),
+        require("none-ls.diagnostics.eslint"),
+        require("none-ls.code_actions.eslint"),
         null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.markdownlint,
