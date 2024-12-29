e763f832c4e43ea60a95c115a2bd9f4f474b16c1
Chris Cardoso <chris.ca@getjobber.com>
2024-12-28 22:10:56 -0500

    feat: disable EslintFixAll on save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -165,12 +165,12 @@ return {
 					settings = {
 						format = false,
 					},
-					on_attach = function(_, bufnr)
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							buffer = bufnr,
-							command = "EslintFixAll",
-						})
-					end,
+					-- on_attach = function(_, bufnr)
+					-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+					-- 		buffer = bufnr,
+					-- 		command = "EslintFixAll",
+					-- 	})
+					-- end,
 				},
 			}
 
