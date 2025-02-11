46a5d0ce8f128012057dec39e7cff6a1727821fc
Chris Cardoso <contact@christophercardoso.dev>
2025-02-11 10:50:46 -0500

    feat: remove ts_ls and use vtsls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -161,15 +161,66 @@ return {
 				prismals = {},
 				sqlls = {},
 				tailwindcss = {},
+				-- ts_ls = {
+				-- 	settings = {
+				-- 		maxTsServerMemory = 12288,
+				-- 	},
+				-- 	typescript = {
+				-- 		inlayHints = ts_ls_inlay_hints,
+				-- 	},
+				-- 	javascript = {
+				-- 		inlayHints = ts_ls_inlay_hints,
+				-- 	},
+				-- },
 				ts_ls = {
+					enabled = false,
+				},
+				vtsls = {
+					-- explicitly add default filetypes, so that we can extend
+					-- them in related extras
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+					root_dir = require("lspconfig").util.root_pattern(
+						".git",
+						"pnpm-workspace.yaml",
+						"pnpm-lock.yaml",
+						"yarn.lock",
+						"package-lock.json",
+						"bun.lockb"
+					),
 					settings = {
-						maxTsServerMemory = 12288,
-					},
-					typescript = {
-						inlayHints = ts_ls_inlay_hints,
-					},
-					javascript = {
-						inlayHints = ts_ls_inlay_hints,
+						complete_function_calls = true,
+						vtsls = {
+							enableMoveToFileCodeAction = true,
+							autoUseWorkspaceTsdk = true,
+							experimental = {
+								maxInlayHintLength = 30,
+								completion = {
+									enableServerSideFuzzyMatch = true,
+								},
+							},
+						},
+						typescript = {
+							updateImportsOnFileMove = { enabled = "always" },
+							suggest = {
+								completeFunctionCalls = true,
+							},
+							inlayHints = {
+								enumMemberValues = { enabled = true },
+								functionLikeReturnTypes = { enabled = true },
+								parameterNames = { enabled = "literals" },
+								parameterTypes = { enabled = true },
+								propertyDeclarationTypes = { enabled = true },
+								variableTypes = { enabled = false },
+							},
+							maxTsServerMemory = 12288,
+						},
 					},
 				},
 				yamlls = {},
