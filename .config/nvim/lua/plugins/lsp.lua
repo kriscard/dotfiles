c885eb5898e5b518ec6bcd8597a29833f889815d
Chris Cardoso <chris.ca@getjobber.com>
2025-02-11 16:04:06 -0500

    feat: remove nvim-lint to leverage only conform

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -228,7 +228,9 @@ return {
 				eslint = {
 					cmd = { "vscode-eslint-language-server", "--stdio", "--max-old-space-size=12288" },
 					settings = {
-						format = false,
+						-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+						workingDirectories = { mode = "auto" },
+						format = true,
 					},
 					on_attach = function(_, bufnr)
 						vim.api.nvim_create_autocmd("BufWritePre", {
