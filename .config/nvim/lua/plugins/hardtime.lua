96eea2106796cdca8a6b6d7e19835c2d6a69c306
Chris Cardoso <contact@christophercardoso.dev>
2025-05-13 16:23:04 -0400

    feat: add nvim hardtime

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,19 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+		disabled_keys = {
+			["<Up>"] = {},
+			["<Space>"] = { "n", "x" },
+		},
+		hints = {
+			["d[tTfF].i"] = {
+				message = function(keys)
+					return "Use c" .. keys:sub(2, 3) .. " instead of " .. keys
+				end,
+				length = 4,
+			},
+		},
+	},
+}
