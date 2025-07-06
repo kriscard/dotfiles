8659e14497acdbcf1c799a3087429975188965d2
Chris Cardoso <contact@christophercardoso.dev>
2025-07-06 19:22:50 -0400

    feat: update lsp config and disable avante

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -3,6 +3,7 @@ return {
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = false,
+		enabled = false,
 		version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
 		opts = {
 			-- add any opts here
