19ae84b332acb6039bbab1d421fc2d3fc1923af4
Christopher Cardoso <christopher.cardoso@roofr.com>
2025-10-21 16:42:55 -0400

    fix: copilot nodejs version hack + add tmux yank

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,7 @@ return {
 		event = "InsertEnter",
 		config = function()
 			require("copilot").setup({
+				copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/22.20.0/bin/node", -- Hardcoded to v22 for copilot while projects can use different versions
 				suggestion = {
 					enabled = true,
 					auto_trigger = true,
