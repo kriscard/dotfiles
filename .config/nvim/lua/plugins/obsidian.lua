20de830966df4cf55cf3d997e1ac22426476813a
Chris Cardoso <contact@christophercardoso.dev>
2025-11-04 10:58:47 -0500

    fix: obsidian daily note

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -36,18 +36,14 @@ return {
 			},
 			templates = {
 				subdir = "Templates",
-				date_format = "%Y-%m-%d-%a",
+				date_format = "%Y-%m-%d",
 				time_format = "%H:%M",
 			},
 			daily_notes = {
-				-- Optional, if you keep daily notes in a separate directory.
 				folder = "1 - Notes/Daily Notes",
-				-- Optional, if you want to change the date format for the ID of daily notes.
 				date_format = "%Y-%m-%d",
-				-- Optional, if you want to change the date format of the default alias of daily notes.
-				alias_format = "%B %-d, %Y",
-				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
 				template = "Daily Notes.md",
+				default_tags = { "daily" },
 			},
 
 			picker = {
